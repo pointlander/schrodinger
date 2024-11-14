426d57ed45b2382cb2b8898b31d197e2af3da0f8
Andrew Snodgrass <pointlander@gmail.com>
2024-11-14 13:55:34 -0600

    Fixed bug

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"math/cmplx"
 )
 
-func Trapezoidal(x, f []complex128) complex128 {
+func Trapezoidal(x, f []float64) float64 {
 	n := len(x)
 	switch {
 	case len(f) != n:
@@ -18,7 +18,7 @@ func Trapezoidal(x, f []complex128) complex128 {
 		panic("integrate: input data too small")
 	}
 
-	integral := complex128(0.0)
+	integral := 0.0
 	for i := 0; i < n-1; i++ {
 		integral += 0.5 * (x[i+1] - x[i]) * (f[i+1] + f[i])
 	}
@@ -26,36 +26,37 @@ func Trapezoidal(x, f []complex128) complex128 {
 	return integral
 }
 
-func Trapezoidal2(x, f []complex128, h float64) complex128 {
+func Trapezoidal2(x, f []float64, h float64) float64 {
 	n := len(f)
 	x_mean := 0.5*f[0] + 0.5*f[n-1]
 	for i := 2; i < n; i++ {
 		x_mean = x_mean + f[i-1]
 	}
-	x_mean = x_mean * complex(h, 0)
+	x_mean = x_mean * h
 	return x_mean
 }
 
 func main() {
-	x := []complex128{0, 1, 2, 3, 4}
-	f := func(x complex128) complex128 {
-		return cmplx.Exp(x + x*1i)
+	x := []float64{0, .1, .2, .3, .4, .5, .6, .7, .8, .9}
+	f := func(x float64) complex128 {
+		return cmplx.Exp(complex(x, 0) * 1i)
 	}
-	y := []complex128{}
+	y := []float64{}
 	for _, v := range x {
-		y = append(y, f(v))
+		yy := cmplx.Abs(f(v))
+		y = append(y, yy)
 	}
 
-	t := func(x complex128) complex128 {
-		return (1.0/2.0 - 1i/2.0) * f(x)
+	t := func(x float64) float64 {
+		return x
 	}
-	fmt.Println(t(4) - t(0))
+	fmt.Println(t(.9) - t(0))
 
 	integral := Trapezoidal(x, y)
 	fmt.Println("Integral:", integral)
 
-	rmin, rmax := 0.0, 4.0
-	ninter1 := 5.0
+	rmin, rmax := 0.0, .9
+	ninter1 := float64(len(x))
 	h := (rmax - rmin) / ninter1
 	integral2 := Trapezoidal2(x, y, h)
 	fmt.Println("Integral2:", integral2)
